f622b3bf70ce804e608b181472dd62f6b84c329b
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-03-21 22:15:52 +0900

    ch02

--- ch02/img_ops.py
+++ ch02/img_ops.py
@@ -6,7 +6,7 @@ import cv2
 img1 = np.empty((240, 320), dtype=np.uint8)       # grayscale image
 img2 = np.zeros((240, 320, 3), dtype=np.uint8)    # color image
 img3 = np.ones((240, 320), dtype=np.uint8) * 255  # dark gray
-img4 = np.full((240, 320, 3), (0, 255, 255), dtype=np.uint8)  # yellow
+img4 = np.full((240, 320, 3), 128, dtype=np.uint8)  # yellow
 
 cv2.imshow('img1', img1)
 cv2.imshow('img2', img2)
